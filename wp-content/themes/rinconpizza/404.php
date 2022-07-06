d38ece71790e3f8c49e9f544a0b441c160ef65b6
Ethel Videa <100855553+EthelVidea20@users.noreply.github.com>
2022-07-06 17:39:38 -0600

    diseño de 404

--- wp-content/themes/rinconpizza/404.php
+++ wp-content/themes/rinconpizza/404.php
@@ -2,8 +2,9 @@
 
 <main class='container'>
  <div class="pagina404 my-5">
+   <img src="/assets/img/404img.png" alt="erro de pagina 404">
     <H1>404 Página no Encontrada</H1>
-    <h2>Click <a href="<?php echo home_url()?>"> aquí para volver a página principal</a></h2>
+    <h2>Click <a href="<?php echo home_url();?>"> aquí para volver a página principal</a></h2>
  </div>
 </main>
 <?php  get_footer() ?>
