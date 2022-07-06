e1185984a9073471fe8118b622cb5d8030465bed
Ethel Videa <100855553+EthelVidea20@users.noreply.github.com>
2022-07-06 17:26:42 -0600

    404

---
+++ wp-content/themes/rinconpizza/404.php
@@ -0,0 +1,9 @@
+<?php get_header() ?>
+
+<main class='container'>
+ <div class="pagina404 my-5">
+    <H1>404 Página no Encontrada</H1>
+    <h2>Click <a href="<?php echo home_url()?>"> aquí para volver a página principal</a></h2>
+ </div>
+</main>
+<?php  get_footer() ?>
